c22fcc7df2af08e1cc80d62dd41ca29e17d2c062
root <sanchezmarco8882@gmail.com>
2024-12-04 16:28:05 -0400

    primera prueba

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('posts', App\Http\Controllers\PostController::class)->only('index', 'store');
